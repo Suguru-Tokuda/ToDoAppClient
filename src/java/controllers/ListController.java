6fc9cc7b89e798238f28986f5b733b18ccb6f29d
suguru-tokuda <suguru.tokuda@gmail.com>
2017-12-09 21:24:59 -0600

    First commit

---
+++ src/java/controllers/ListController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ * This controller handles all the list related activities.
+ * 3. Create and Add to List
+ * 5. Sync with Cloud
+ * 6. Prioritize List Items
+ * 7. Support Multiple Lists
+ * 8. Build List from History
+ * 
+ * @author Suguru
+ */
+public class ListController {
+    
+}
---
+++ src/java/controllers/UserController.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ * This controller handles all the user related activities.
+ * 1. Register for Account
+ * 2. Sign in
+ * 4. Invite a (Family) Member to List
+ * @author Suguru
+ */
+public class UserController {
+    
+}
